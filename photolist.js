615a19d3f8f5d611f55c4afa58afdb692f325fc9
Midokhalil1 <midokhalil22@gmail.com>
2022-11-10 09:51:15 -0500

    updating photos

---
+++ photolist.js
@@ -0,0 +1,57 @@
+//put fake photolist instead on api
+
+const photolist = [
+    
+    { 
+        id:1,
+        photoURL :'https://images.unsplash.com/photo-1667335630421-3cad08d5f139?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY2ODA5MTMyNg&ixlib=rb-4.0.3&q=80&w=1080',
+        description: 'camera on tripod',
+        userName: '',
+profilePhoto: '',
+
+},
+{
+
+id:2,
+photoURL :'https://images.unsplash.com/photo-1667335630421-3cad08d5f139?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY2ODA5MTMyNg&ixlib=rb-4.0.3&q=80&w=1080',
+description: 'climbing ',
+userName: '',
+profilePhoto: '',
+
+},
+
+{
+id:3,
+photoURL :'https://images.unsplash.com/photo-1666636269834-e8d4e1b7bd08?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY2ODA5MTU2Mg&ixlib=rb-4.0.3&q=80&w=1080https://images.unsplash.com/photo-1667335630421-3cad08d5f139?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY2ODA5MTMyNg&ixlib=rb-4.0.3&q=80&w=1080',
+description: 'in the wild ',
+userName: '',
+profilePhoto: '',
+
+},
+
+{
+id:4,
+    photoURL :'https://images.unsplash.com/photo-1666044443668-a80ea222040a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY2ODA5MTUzNQ&ixlib=rb-4.0.3&q=80&w=1080https://images.unsplash.com/photo-1667335630421-3cad08d5f139?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY2ODA5MTMyNg&ixlib=rb-4.0.3&q=80&w=1080',
+    description: 'deep thinking ',
+    userName: '',
+profilePhoto: '',
+    
+    },
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
+
---
+++ styles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create ({
+
+    feed: {
+        flex: 1,
+        backgroundColor: 'white',
+
+
+    }
+
+
+})
+
+export default styles;
